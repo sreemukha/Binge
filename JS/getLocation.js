62ab507b8566b0fc506b52af51ebb9ee68188416
sreemukha <sreemukha.mail@gmail.com>
2017-04-18 02:19:24 -0400

    Cleaned up index.html and JS

--- JS/getLocation.js
+++ JS/getLocation.js
@@ -1,5 +1,18 @@
 
-var x = document.getElementById("search");
+var input = document.getElementById("search");
+function initialize() {
+            var options = {
+            types: ['(cities)'],
+            componentRestrictions: {country: "us"}
+            };
+            //var input = document.getElementById('search');
+            var autocomplete = new google.maps.places.Autocomplete(input,options);
+            autocomplete.addListener('place_changed', function () {
+            var place = autocomplete.getPlace();
+            });
+             
+}
+google.maps.event.addDomListener(window, 'load', initialize); 
 
 window.onload = function getLocation() {
     if (navigator.geolocation) {
@@ -7,7 +20,7 @@ window.onload = function getLocation() {
         
     } else { 
         
-        x.value = "Geolocation is not supported by this browser.";
+        input.value = "Geolocation is not supported by this browser.";
     }
 }
 
@@ -19,7 +32,7 @@ function showPositionLocality(position) {
     geocoder.geocode({'location': latlng}, function(results, status) {
     if (status === 'OK') {
       if (results[1]) {
-        x.value = results[1].formatted_address;
+        input.value = results[1].formatted_address;
         
       } else {
         window.alert('No results found');
@@ -30,29 +43,10 @@ function showPositionLocality(position) {
   });
 }
 
-document.getElementById("search").focus;
-
 function showError(error) {
     switch(error.code) {
         case error.PERMISSION_DENIED:
             window.alert("Please enter your location");
-//            
-//            function initialize() {
-//            var options = {
-//            types: ['(cities)'],
-//            componentRestrictions: {country: "us"}
-//            };
-//            var input = document.getElementById('search');
-//            var autocomplete = new google.maps.places.Autocomplete(input[0],options);
-//            /*autocomplete.addListener('place_changed', function () {
-//            var place = autocomplete.getPlace();
-//            // place variable will have all the information you are looking for.
-//            console.log(place.geometry['location'].lat());
-//            console.log(place.geometry['location'].lng());
-//            });*/
-//            
-//        }
-//        google.maps.event.addDomListener(window, 'click', initialize);
             break;
         case error.POSITION_UNAVAILABLE:
             window.alert("Location information is unavailable.");
