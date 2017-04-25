f692f7037ddeb4981f693a1d645a5924edb9b73b
vpangana <vpangana@iu.edu>
2017-04-25 16:32:31 -0400

    added http post request

--- JS/getLocation.js
+++ JS/getLocation.js
@@ -1,6 +1,13 @@
 
 var input = document.getElementById("search");
+var geocoder = new google.maps.Geocoder;
+input.addEventListener("keydown", function(addr){
+		if(addr.keyCode === 13){
+			geocodeAdress();
+		}
+	})
 
+// This function handles the auto-fill of the drop down associated with the search box
 function initialize() {
             var options = {
             types: ['(cities)'],
@@ -15,6 +22,7 @@ function initialize() {
 }
 google.maps.event.addDomListener(window, 'load', initialize); 
 
+// This is the function that asks the user for location access
 window.onload = function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPositionLocality, showError);
@@ -25,10 +33,10 @@ window.onload = function getLocation() {
     }
 }
 
-
+// This function displays the current location in terms of formatted address
 function showPositionLocality(position) {
     
-    var geocoder = new google.maps.Geocoder;
+    
     var latlng = {lat: position.coords.latitude, lng: position.coords.longitude}
     geocoder.geocode({'location': latlng}, function(results, status) {
     if (status === 'OK') {
@@ -44,6 +52,7 @@ function showPositionLocality(position) {
   });
 }
 
+// This function handles the errors that ccur due to issues caused by google maps api 
 function showError(error) {
     switch(error.code) {
         case error.PERMISSION_DENIED:
@@ -59,4 +68,23 @@ function showError(error) {
             window.alert("An unknown error occurred.");
             break;
     }
+}
+
+//This function is used to convert the addressfield to longitude and latitude values
+function geocodeAdress(){
+
+	var address = input.value;
+	geocoder.geocode({'address' : address}, function(results, status){
+		if (status==='OK') {
+			var params = results[0].geometry.location.toString();
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', 'http://localhost:8080/databasedesign/webapi/myresource', true);
+			xhr.send(params);	
+			//console.log(results[0].geometry.location.toString());
+		}
+		else{
+			alert("Geocode was not successful for the following reason: " + status);
+		}
+	})
+
 }
